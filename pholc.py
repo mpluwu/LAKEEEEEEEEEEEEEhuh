9cfc6969faf077fbc5c7dcaad19e38f497780644
pluto/star <56968613+mpluwu@users.noreply.github.com>
2023-12-21 12:41:21 +0400

    Update pholc.py

--- pholc.py
+++ pholc.py
@@ -1,18 +1,20 @@
 ph=[5.5,	9,	8.4,	9,	8.5,	8.5,	8.5,8.4,	8.3,	8.3,	8.3,	8.4,	8.3,	8.3		]
-phnorm=[6.5,8.5]
+phnorm=[6.5,8.5,6.6,9,3,8,1,10,6,90,2,45,7,90]
 timeline=[]
 def qiymet(n):
     n=n-1
     timeline.append(ph[n:n+7])
 def phyoxlama(n):
     n=n-1
+    p=0
     phnetice=[]
     abc=timeline[n]
     for i in range(len(abc)):
-        if phnorm[0]<= abc[i]<=phnorm[1]:
+        if phnorm[p]<= abc[i]<=phnorm[p+1]:
             phnetice.append(abc[i])
         else:
             phnetice.append('normadan kenar')
+        p+=2
     print(phnetice)
     if 'normadan kenar' in phnetice:
         a=input('Menteqelere melumat gonderilsinmi?')
@@ -26,9 +28,21 @@ def phyoxlama(n):
                 elif phnetice.index('normadan kenar')==1:
                     print("Su goturucu qurgunun adi")
                     phnetice[1]='temir'
+                elif phnetice.index('normadan kenar')==2:
+                    print("Cenub nasos stansiyasinin yani")
+                    phnetice[2]='temir'
                 elif phnetice.index('normadan kenar')==3:
                     print("Cenub-qerb dambasi")
                     phnetice[3]='temir'
+                elif phnetice.index('normadan kenar')==4:
+                    print("Cenub dambasi")
+                    phnetice[4]='temir'
+                elif phnetice.index('normadan kenar')==5:
+                    print("Simal serq dambasi")
+                    phnetice[5]='temir'
+                elif phnetice.index('normadan kenar')==6:
+                    print("meliorasiya")
+                    phnetice[6]='temir'
         else:
             print('Neticede filan filan xestelikler olacaq')
     print(phnetice)
@@ -36,4 +50,5 @@ a=int(input())
 qiymet(a)
 
 print(timeline)
+phyoxlama(a)
 phyoxlama(a)
